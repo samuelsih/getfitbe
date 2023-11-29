0b25d9ed19122acfd1012027c125b8bf496a8b5d
samuelsih <samuel.205025@mhs.its.ac.id>
2023-11-29 14:35:46 +0700

    feat: auth security

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { customValidationPipe } from './config';
 import { AllExceptionFilter } from './exception/all.filter';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS'],
+  });
+  app.use(helmet());
 
   app.useGlobalPipes(customValidationPipe);
   app.useGlobalFilters(new AllExceptionFilter());
